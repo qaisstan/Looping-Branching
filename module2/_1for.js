6d2321966a186707ce00607b8582f616234fb97e
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-27 19:01:49 +0100

    Loops

--- module2/_1for.js
+++ module2/_1for.js
@@ -1,3 +1,4 @@
+// For Loops
 console.log("Classic loop");
 for (let step = 0; step <= 4; step++) {
   console.log("Walking east one step: " + step);
@@ -6,4 +7,21 @@ for (let step = 0; step <= 4; step++) {
 console.log("Going Backwards");
 for (let step = 5; step > 0; step--) {
   console.log("Walking Backward one step:" + step);
+}
+
+console.log("Going every nth element");
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (let i = 1; i < array.length / 2; i = i + 2) {
+  console.log("Every Second element " + i);
+}
+
+// For Off
+const arr = [10, 20, 30];
+for (let value of arr) {
+  console.log(value);
+}
+
+// index value pair
+for (let [index, value] of arr.entries()) {
+  console.log(index, value);
 }
