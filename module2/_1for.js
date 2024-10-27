7ab1a143d4b428c5db0d9957bf4bcc215773790e
qaisstan <94216834+qaisstan@users.noreply.github.com>
2024-10-27 18:29:02 +0100

    first commit

---
+++ module2/_1for.js
@@ -0,0 +1,9 @@
+console.log("Classic loop");
+for (let step = 0; step <= 4; step++) {
+  console.log("Walking east one step: " + step);
+}
+
+console.log("Going Backwards");
+for (let step = 5; step > 0; step--) {
+  console.log("Walking Backward one step:" + step);
+}
